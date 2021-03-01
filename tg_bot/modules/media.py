a1373eeb08c1a4074bedc47ae59f14f750031939
Francesco <60934657+FraSharp@users.noreply.github.com>
2021-03-01 17:44:20 +0100

    kang media from astrakobot

---
+++ tg_bot/modules/media.py
@@ -0,0 +1,18 @@
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot import dispatcher
+
+from telegram.ext import CallbackContext, Filters, CommandHandler
+
+__help__ = """
+*Available commands:*\n
+*Image reverse:*
+ • `/reverse`: does a *reverse image search* of the media which it was replied to\n
+*Text to speech:*
+ • `/tts <text>`: convert text to speech\n
+*Songs:*
+ • `/song <songname artist(optional)>`: uploads the song in it's best quality available
+ • `/video <songname artist(optional)>`: uploads the video song in it's best quality available\n
+"""
+
+__mod_name__ = "Media"
+__command_list__ = ["reverse", "tts", "song", "video"]
